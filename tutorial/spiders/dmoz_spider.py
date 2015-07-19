cfccc6e123c07baa2096edd2aabc71e832732a8c
c0cky <camron248@gmail.com>
2015-07-18 18:32:00 -0700

    add base url

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -214,6 +214,8 @@ class DmozSpider(scrapy.Spider):
 'http://mirc.sc.edu/islandora/search/catch_all_txt%3A%28*%3A*%29?page=206' ]
 
 def parse(self, response):
+    BASE_URL = 'http://mirc.sc.edu/'
+
     for sel in response.xpath('//div[@class="islandora-solr-search-result-inner"]'):
         item = DmozItem()
         item['image_urls'] = sel.xpath('dl/dt/a/img/@src').extract()
